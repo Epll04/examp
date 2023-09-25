944ba48f232da6e3d79fbf85dff5830ce72970ea
Epll <epll@yandex.ru>
2023-09-25 10:09:46 +0300

    add random3

--- Examp_3_Summ/Program.cs
+++ Examp_3_Summ/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberA = new Random().Next(1, 10); // 0,1,2,3,4,5,6,7,8,9
-Console.WriteLine(numberA);
+Console.WriteLine(numberA); // 0,1,2,3,4,5,6,7,8,9
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
